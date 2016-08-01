af1e386ea61fe3f8f47b0a0cc202d35247e736af
U-CRITEOIS\a.careil <a.careil@FR-8PWYBB2.criteois.lan>
2016-08-01 16:25:41 +0200

    Fix windows specific config

--- .init/global.el
+++ .init/global.el
@@ -10,11 +10,16 @@
 ;; la différenciation de deux fichiers avec diff
 (setq-default ediff-ignore-similar-regions t)
 
-;; Navigateur textuel
-(require 'w3m)
-
+(use-package w3m
+  :init (if (eq system-type 'windows-nt) (setq w3m-command "C:\\cygwin\\bin\\w3m.exe"))
+  :diminish w3m-mode 
+  )
+  
+(use-package beacon
+	:init (beacon-mode 1)
+)
+  
 (use-package smartparens
-  :ensure t
   :diminish smartparens-mode
   :config
   (progn
@@ -25,7 +30,6 @@
   )
 
 (use-package helm-dash
-  :ensure t
   :config (progn
             (setq helm-dash-docsets-path "~/.emacs.d/docsets" )
             (setq browse-url-browser-function 'w3m)
@@ -34,24 +38,19 @@
   )
 
 (use-package yasnippet
-  :ensure t
   :diminish yas-minor-mode
-  :mode ("/\\.emacs\\.d/snippets/" . snippet-mode)
   :init
   (progn
-    (setq yas-verbosity 3)
-    (yas-global-mode 1)))
+    (setq yas-verbosity 3)))
 
 (use-package projectile
   :init (progn
-          (projectile-global-mode 1)
           (add-hook 'projectile-mode-hook 'projectile-rails-on)
           )
   )
 
 (use-package company
   :diminish company-mode
-  :init (global-company-mode 1)
   :config (progn
             (require 'color)
             
@@ -67,7 +66,7 @@
 
 (use-package flycheck
   :init (progn
-          (global-flycheck-mode 1)
+          (global-flycheck-mode)
           )
-  ;;:config (setq-default flycheck-disabled-checkers '(c/c++-clang))
+  :config (setq-default flycheck-disabled-checkers '(c/c++-clang))
   )
--- init.el
+++ init.el
@@ -4,22 +4,30 @@
         (message "Emacs init file saved and compiled.")))
 (defun my-emacs-lisp-mode-hook ()
 
+
   ;; Load paths 
   (add-to-list 'load-path "~/.emacs.d/elpa")
   (add-to-list 'custom-theme-load-path
-             (file-name-as-directory "/home/alexandre/.emacs.d/perso/themes"))
+             (file-name-as-directory "~/.emacs.d/perso/themes"))
+			 
+  (if (eq system-type 'windows-nt)
+    (progn 
+      (setenv "PATH" (concat "c:/cygwin/bin;" (getenv "PATH")))
+      (setq exec-path (cons "c:/cygwin/bin/" exec-path))
+      (require 'cygwin-mount)
+      (cygwin-mount-activate)))
 
   (load "~/.emacs.d/.init/general.el")
   (load "~/.emacs.d/.init/global.el")
 
   (load "~/.emacs.d/.init/javascript.el")
   (load "~/.emacs.d/.init/python.el")
-  (load "~/.emacs.d/.init/c++.el")
+  ;;(load "~/.emacs.d/.init/c++.el")
   (load "~/.emacs.d/.init/ruby.el")
-  (load "~/.emacs.d/.init/web.el")
+  ;;(load "~/.emacs.d/.init/web.el")
   (load "~/.emacs.d/.init/org.el")
   (load "~/.emacs.d/.init/latex.el")
-  (load "~/.emacs.d/.init/csharp.el")
+  ;;(load "~/.emacs.d/.init/csharp.el")
 
   ;; Themes
   (load-theme 'jazz t t)
@@ -34,13 +42,24 @@
 (add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-hook)
 
 (custom-set-variables
-;; ;; custom-set-variables was added by Custom.
-;; ;; If you edit it by hand, you could mess it up, so be careful.
-;; ;; Your init file should contain only one such instance.
-;; ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
-)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(magit-auto-revert-mode nil))
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-scrollbar-bg ((t (:background "#ffffff"))))
+ '(company-scrollbar-fg ((t (:background "#ffffff"))))
+ '(company-tooltip ((t (:inherit font-lock-comment-face :background "#ffffff"))))
+ '(company-tooltip-common ((t (:inherit font-lock-string-face :background "#ffffff"))))
+ '(company-tooltip-selection ((t (:inherit font-lock-keyword-face :background "#ffffff")))))
